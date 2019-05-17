323b57ddeb660d1076bdeb12843c400bcab0825f
Loshgoobi <dozoulremy@gmail.com>
2019-05-17 15:20:30 +0200

    add login

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { createStackNavigator } from 'react-navigation';
 
 import Chat from './components/Chat';
+import Login from './components/Login';
 
 export default createStackNavigator({
+  Login: { screen: Login },
   Chat: { screen: Chat }
 });
